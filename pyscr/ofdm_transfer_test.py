38685046ce926518c2e29d77d07d43527216a1d8
osherlock1 <omalley.j.sherlock@gmail.com>
2025-10-22 19:19:22 -0400

    Add payload estimation

--- pyscr/ofdm_transfer_test.py
+++ pyscr/ofdm_transfer_test.py
@@ -5,6 +5,7 @@ from subcarrier_map import SubcarrierMap
 import matplotlib.pyplot as plt
 from data_generator import DataGenerator
 import random
+import scipy
 
 def main():
     
@@ -14,7 +15,9 @@ def main():
     data_gen = DataGenerator()
 
     #Genearte random OFDM pakcet
-    packet = data_gen.generate_random_packet(N_data_symbols= 6)
+    N_data_symbols = 6
+    N_symbols = N_data_symbols + 1
+    packet = data_gen.generate_random_packet(N_data_symbols= N_data_symbols)
 
     #Add Noise Buffer
     noise_buffer = generate_noise_buffer(100)
@@ -28,17 +31,48 @@ def main():
         P, R, M = om.schmidl_cox_metrics_P_R_M(r = packet, delay=delay_i)
         M_values.append(M)
 
+    #Filter M values
+    ofdm_CP = 8
+    b_toPeak = np.ones(ofdm_CP) / ofdm_CP
+    a = (1,)
+    M_filter = scipy.signal.lfilter(b_toPeak, a, M_values)
+    
+    #Calculate derivative of filtered M
+    D = np.diff(M_filter)
+
+    #Find zero crossings
+    zeroCrossing_1 = (D[:1] * D[1:]) <= 0
+    M_values_np = np.array(M_values, dtype=complex)
+    zeroCrossing_2 = ((D[:-1] * D[1:]) <= 0) * (M_values_np[1:-1] > 0.5)
+
+    #Remove duplicate zero crossings
+    b_preamble = np.zeros(map.N + N_symbols * (map.N + ofdm_CP))
+    b_preamble[:map.N] = 1
+
+    b_payload = np.zeros(map.N + N_symbols*(map.N + ofdm_CP))
+    for s in range(N_data_symbols):
+        b_payload[map.N + (s + 1)*ofdm_CP + s * map.N + np.arange(map.N)] = 1
+
+    preamble_valid_est = scipy.signal.lfilter(b_preamble, (1,), zeroCrossing_2)
+    payload_valid_est = scipy.signal.lfilter(b_payload, (1,), zeroCrossing_2)
+
     #Plot the OFDM packet
     plt.figure()
-    plt.plot(packet)
-    plt.plot(M_values)
+    plt.plot(packet, label = "OFDM Packet")
+    plt.plot(M_values, label = "M Values")
+    plt.plot(M_filter, label = "Filtered M")
+    plt.plot(D, label = "Derivative of M_filter")
+    plt.plot(zeroCrossing_2, label = "zero crossings")
+    plt.plot(preamble_valid_est, label = "Estiamtion of valid Sync")
+    plt.plot(payload_valid_est, label = "Estimation of valid payload")
+    plt.legend()
     plt.show()
 
 
 def generate_noise_buffer(buf_len:int = 200) -> np.ndarray:
     noise_buffer = []
     for i in range(buf_len):
-        noise_buffer.append(random.uniform(-0.3,0.3))
+        noise_buffer.append(random.uniform(-0.1,0.1))
     return np.array(noise_buffer, dtype=complex)
 
 if __name__ == "__main__":
