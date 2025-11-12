f47f995ca402cee8c384f381c76a25139e8c321b
osherlock1 <omalley.j.sherlock@gmail.com>
2025-11-12 18:09:46 -0500

    update cfo calculation

--- pyscr/cfo_test.py
+++ pyscr/cfo_test.py
@@ -28,15 +28,54 @@ data_idx = np.array([map.idx(k) for k in data_k])
 
 Tx = packet[-64:]
 Tx_data = Tx[data_idx]
-cfo = 12327
-Rx_data = Tx_data * ((np.e ** (-1j * cfo)))
+cfo = 23423
 
 
-correlation = correlate((Tx_data), np.conj((Rx_data)), mode='full')
-correlation /= np.sqrt(np.dot(Tx_data, Tx_data) * (np.dot(Rx_data, Rx_data)))
+fs = 100e3
+bins = 2 ** 12
+f_grid = np.linspace(-fs/2, fs/2, bins, endpoint = False)
+n = len(Tx_data)
+print(n)
 
-corr_sum = np.sum(correlation)
-print(np.abs(corr_sum))
+n = np.arange(n)
+
+
+
+#print(t)
+
+Rx_data = Tx_data * np.exp(1j * 2*np.pi * cfo * n / fs)
+
+frequencies = np.linspace(-fs, fs, bins)
+#print(frequencies)
+
+
+Rx_hat, G, k, f_hat = om.cfo_correct(Tx_data, Rx_data, fs)
+
+# G = np.empty(bins, dtype=np.complex128)
+# for i, f in enumerate(f_grid):
+#     r = Rx_data * np.exp(-1j * 2*np.pi * f * n / fs)
+#     G[i] = np.vdot(Tx_data,r)
+plt.figure()
+plt.plot(f_grid, np.abs(G))
+k = int(np.argmax(np.abs(G) ** 2))
+
+plt.figure()
+plt.plot(Rx_data)
+plt.plot(Rx_hat)
+
+plt.show()
+
+
+
+
+
+
+
+#correlation = correlate((Tx_data), np.conj((Rx_data)), mode='full')
+#correlation /= np.sqrt(np.dot(Tx_data, Tx_data) * (np.dot(Rx_data, Rx_data)))
+
+#corr_sum = np.sum(correlation)
+#print(np.abs(corr_sum))
 
 """
 
