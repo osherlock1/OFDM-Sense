7b7c81b5065dfc07f5fec68395da65fb5a0eab96
osherlock1 <omalley.j.sherlock@gmail.com>
2025-11-20 17:49:51 -0500

    add semi-random pilot indicies generation

--- pyscr/subcarrier_map.py
+++ pyscr/subcarrier_map.py
@@ -1,6 +1,26 @@
+import random
+import numpy as np
+
 class SubcarrierMap:
     N: int = 64
-    pilots_k = (-21, -7, 7, 21)
+    num_pilots = 12
+    random.seed(42)
+
+
+    @property
+    def pilots_k(self):
+        random.seed(42)
+        used_neg = list(range(-26, 0))
+        used_pos = list(range(1,27))
+        pilot_bins = []
+        used_p_bins = used_neg + used_pos
+
+        for k in range(self.num_pilots):
+            random_k = random.choice(used_p_bins)
+            pilot_bins.append(random_k)
+        return pilot_bins
+
+
     cp_len = 8
     def idx(self, k:int) -> int:
         return k % self.N
