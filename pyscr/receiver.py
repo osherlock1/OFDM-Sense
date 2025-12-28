712c0b9c69474fcd658d5132f903b932dde1cafb
osherlock1 <omalley.j.sherlock@gmail.com>
2025-12-28 14:59:55 -0500

    initialize ursp module

--- pyscr/receiver.py
+++ pyscr/receiver.py
@@ -1,3 +1,5 @@
+import sys
+import pathlib
 import subprocess, pathlib, shlex, sys, time
 import numpy as np
 import os, sys, numpy as np
@@ -13,6 +15,8 @@ from pilot_symbol import PilotSymbol
 import argparse
 from sync_symbol import SyncSymbol
 from dataclasses import dataclass
+from ofdm.utils import usrp
+
 
 #------Global Class Def ----------
 #Helper Classes Instantiation
@@ -65,85 +69,107 @@ def main():
     SAMPLE_BUFFER = 500 #Number of additional samples to transfer from the length of the OFDM packet
     FS = 100e6
     #CPP ARGS
-    BUILD_PATH = "./build/TXRX_FROM_FILE"
-    TX_ADDR = "addr=192.168.30.2"
-    RX_ADDR = "addr=192.168.30.2"
-    TX_RATE = str(FS)
-    RX_RATE = str(FS)
-    TX_FREQ = "60e6"
-    RX_FREQ = "60e6"
-    WAVE_TYPE = "SINE"
-    WAVE_FREQ = "100e3"
-    AMPL = "0.3"
-    TX_GAIN = "0"
-    RX_GAIN = "0"
-    OTW = "sc16"
-    TYPE = "float"
-    FILE_NAME = FileConfig.RX_FILE_PATH
-    NSAMPLES = str(SymbolConfig.n_samples + SAMPLE_BUFFER)
-    SETTLING = "0"
-    TX_FILE = FileConfig.TX_FILE_PATH
-    #TX_FILE = "data_files/ofdm_iq_interleaved.dat" #NAME OF FILE YOU WANT TO READ AND SEND
-    TX_TYPE = "float"
-    TX_SPB = "0"
-    TX_REPEAT = "false"
-    RX_CHANNEL = "0" #Specify number of active channel (0 = one channel, 1 = both (2) channels)
-    TX_CHANNEL = "0"
+    # BUILD_PATH = "./build/TXRX_FROM_FILE"
+    # TX_ADDR = "addr=192.168.30.2"
+    # RX_ADDR = "addr=192.168.30.2"
+    # TX_RATE = str(FS)
+    # RX_RATE = str(FS)
+    # TX_FREQ = "60e6"
+    # RX_FREQ = "60e6"
+    # WAVE_TYPE = "SINE"
+    # WAVE_FREQ = "100e3"
+    # AMPL = "0.3"
+    # TX_GAIN = "0"
+    # RX_GAIN = "0"
+    # OTW = "sc16"
+    # TYPE = "float"
+    # FILE_NAME = FileConfig.RX_FILE_PATH
+    # NSAMPLES = str(SymbolConfig.n_samples + SAMPLE_BUFFER)
+    # SETTLING = "0"
+    # TX_FILE = FileConfig.TX_FILE_PATH
+    # #TX_FILE = "data_files/ofdm_iq_interleaved.dat" #NAME OF FILE YOU WANT TO READ AND SEND
+    # TX_TYPE = "float"
+    # TX_SPB = "0"
+    # TX_REPEAT = "false"
+    # RX_CHANNEL = "0" #Specify number of active channel (0 = one channel, 1 = both (2) channels)
+    # TX_CHANNEL = "0"
 
 
-    if CHANNEL_SEL == "A":
-        print("CHANNEL SELECTED A:0")
-        RX_SUBDEV = "A:0"
-        TX_SUBDEV = "A:0"
-    elif CHANNEL_SEL == "B":
-        print("CHANNEL SELECTED B:0")
-        RX_SUBDEV = "B:0"
-        TX_SUBDEV = "B:0"
-    else:
-        print(f"WARNING: INVALID CHANNEL SELCTION {CHANNEL_SEL}.  SETTING CHANNEL TO B:0")
-        RX_SUBDEV = "B:0"
-        TX_SUBDEV = "B:0"
+    # if CHANNEL_SEL == "A":
+    #     print("CHANNEL SELECTED A:0")
+    #     RX_SUBDEV = "A:0"
+    #     TX_SUBDEV = "A:0"
+    # elif CHANNEL_SEL == "B":
+    #     print("CHANNEL SELECTED B:0")
+    #     RX_SUBDEV = "B:0"
+    #     TX_SUBDEV = "B:0"
+    # else:
+    #     print(f"WARNING: INVALID CHANNEL SELCTION {CHANNEL_SEL}.  SETTING CHANNEL TO B:0")
+    #     RX_SUBDEV = "B:0"
+    #     TX_SUBDEV = "B:0"
 
-    #Build the command to run
-    run_cmd = [
-        BUILD_PATH,
-        "--tx-args", TX_ADDR,
-        "--rx-args", RX_ADDR,
-        "--tx-rate", TX_RATE,
-        "--rx-rate", RX_RATE,
-        "--tx-freq", TX_FREQ,
-        "--rx-freq", RX_FREQ,
-    #   "--wave-type", WAVE_TYPE,
-    #   "--wave-freq", WAVE_FREQ,
-    #   "--ampl", AMPL,
-        "--tx-gain", TX_GAIN,
-        "--rx-gain", RX_GAIN,
-        "--otw", OTW,
-        "--type", TYPE,
-        "--file", FILE_NAME,
-        "--nsamps", NSAMPLES,
-        "--settling", SETTLING,
-        "--tx-file", TX_FILE,
-        "--tx-type", TX_TYPE,
-        "--tx-spb", TX_SPB,
-        "--tx-repeat", TX_REPEAT,
-        "--rx-channels", RX_CHANNEL,
-        "--tx-channels", TX_CHANNEL,
-        "--rx-subdev", RX_SUBDEV,
-        "--tx-subdev", TX_SUBDEV
-    ]
+    # #Build the command to run
+    # run_cmd = [
+    #     BUILD_PATH,
+    #     "--tx-args", TX_ADDR,
+    #     "--rx-args", RX_ADDR,
+    #     "--tx-rate", TX_RATE,
+    #     "--rx-rate", RX_RATE,
+    #     "--tx-freq", TX_FREQ,
+    #     "--rx-freq", RX_FREQ,
+    # #   "--wave-type", WAVE_TYPE,
+    # #   "--wave-freq", WAVE_FREQ,
+    # #   "--ampl", AMPL,
+    #     "--tx-gain", TX_GAIN,
+    #     "--rx-gain", RX_GAIN,
+    #     "--otw", OTW,
+    #     "--type", TYPE,
+    #     "--file", FILE_NAME,
+    #     "--nsamps", NSAMPLES,
+    #     "--settling", SETTLING,
+    #     "--tx-file", TX_FILE,
+    #     "--tx-type", TX_TYPE,
+    #     "--tx-spb", TX_SPB,
+    #     "--tx-repeat", TX_REPEAT,
+    #     "--rx-channels", RX_CHANNEL,
+    #     "--tx-channels", TX_CHANNEL,
+    #     "--rx-subdev", RX_SUBDEV,
+    #     "--tx-subdev", TX_SUBDEV
+    # ]
 
     #----- Check for Simluation ------
-    if SIMULATION is False:
-        print("\n")
-        print(f"Running {BUILD_PATH}...")
-        print(str(run_cmd))
-        subprocess.run(run_cmd)
-        print(f"Run of {BUILD_PATH} complete!")
-        file_name = FileConfig.RX_FILE_PATH
-    else:
-        print("Simulating OFDM Transfer... \n")
-        file_name = FileConfig.TX_FILE_PATH
+    # if SIMULATION is False:
+    #     print("\n")
+    #     print(f"Running {BUILD_PATH}...")
+    #     print(str(run_cmd))
+    #     subprocess.run(run_cmd)
+    #     print(f"Run of {BUILD_PATH} complete!")
+    #     file_name = FileConfig.RX_FILE_PATH
+    # else:
+    #     print("Simulating OFDM Transfer... \n")
+    usrp_conf = usrp.USRPConfig(
+        tx_freq=60e6,
+        rx_freq=60e6,
+        tx_rate=100e6,
+        rx_rate=100e6,
+        tx_gain=0,
+        rx_gain=0
+    )
+
+    SAMPLE_BUFFER = 500
+    total_samples = SymbolConfig.n_samples + SAMPLE_BUFFER
+
+    usrp.run_transfer(
+        config=usrp_conf,
+        tx_file=FileConfig.TX_FILE_PATH,
+        rx_file=FileConfig.RX_FILE_PATH,
+        nsamps = total_samples,
+        simulate=SIMULATION
+    )
+
+
+
+    file_name = FileConfig.TX_FILE_PATH
 
     #------- Unpack Reciever IQ sampels---------
     print("Unpacking OFDM Symbol....")
