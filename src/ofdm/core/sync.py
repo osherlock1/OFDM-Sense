338e83eea43a9e5bbbcb10fb20a1bfee5e1d82cb
osherlock1 <omalley.j.sherlock@gmail.com>
2025-12-29 21:44:04 -0500

    update sync module

--- src/ofdm/core/sync.py
+++ src/ofdm/core/sync.py
@@ -14,22 +14,20 @@ def calculate_schmidl_cox_metrics(rx_signal: np.ndarray, config:OFDMConfig)->Tup
 
     L = config.N // 2
 
-    #Energy Calc
-    #R is moving sum of energy of the second half of the window
-    energy = np.abs(rx_signal) ** 2
-
-    R = scipy.signal.fftconvolve(energy, np.ones(L), mode='valid')
-
     #Correlate the signal with a version of itself shifted by L
     r_upper = rx_signal[L:]
     r_lower = rx_signal[:-L]
 
     #Multiply elemeent wise
-    mult_term = np.conj(r_lower * r_upper)
+    mult_term = np.conj(r_lower) * r_upper
 
     #Moving sum over window L
     P = scipy.signal.fftconvolve(mult_term, np.ones(L), mode='valid')
 
+    #Calculate R
+    energy = np.abs(r_upper) ** 2
+    R = scipy.signal.fftconvolve(energy, np.ones(L), mode='valid')
+    
     #Compute M Metric
     min_len = min(len(P), len(R))
     P = P[:min_len]
@@ -38,14 +36,38 @@ def calculate_schmidl_cox_metrics(rx_signal: np.ndarray, config:OFDMConfig)->Tup
     #Avoid division by zero
     R[R == 0] = 1e-10
 
-    #M = |P|^2 / R^2
-    M = (np.abs(P) ** 2) / (R ** 2)
+    #Calculate M = |P|^2 / R^2 with energy threshhold
+    M = np.zeros_like(R)
 
+    #Calculate nosie floor (cut out anything below 10% of avg power)
+    avg_energy = np.mean(R)
+    energy_threshold = avg_energy * 0.1
+
+    valid_indicies = R > energy_threshold
+
+
+
+    M[valid_indicies] = (np.abs(P[valid_indicies]) ** 2) / (R[valid_indicies] ** 2)
     return M, P
-    #Plot original siganl
-    # plotter.plot_time_series(signal=rx_signal, title="Original Signal")
-    # plotter.plot_time_series(signal=energy, title="Energy")
-    # plotter.plot_time_series(signal=R, title="R")
-    # plotter.plot_time_series(signal=P, title="P")
-    # plotter.plot_time_series(signal=M, title="M")
-    # plt.show()
+
+
+def find_start_idx(M: np.ndarray, config: OFDMConfig, threshold: float = 0.8) -> int:
+    """  
+    Find the start of the packet based on the M metric
+    """
+
+    canidates = np.where(M > threshold)
+
+    if len(canidates) == 0:
+        #Pick max
+        start_idx = np.argmax(M)
+        return start_idx
+    
+    peak_idx = np.argmax(M)
+    return peak_idx
+
+def estimate_cfo_coarse(P_value: complex, config: OFDMConfig) -> float:
+    #Calculate Phase difference with P metrics
+    theta = np.angle(P_value)
+    cfo_hz = (theta * config.FS) / (np.pi * config.N)
+    return cfo_hz
