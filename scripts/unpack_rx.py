30e45f508288ce386cc92b6e73f4a8c5b176a0bd
osherlock1 <omalley.j.sherlock@gmail.com>
2026-01-02 14:11:54 -0500

    fix cp_len

--- scripts/unpack_rx.py
+++ scripts/unpack_rx.py
@@ -94,7 +94,7 @@ def main():
 
     #Get TX pilot Ref
     tx_pilot_ref = np.array(ref_data['pilot_ref_real']).astype(complex) + 1j * np.array(ref_data['pilot_ref_imag']).astype(complex)
-    tx_pilot_no_cp = waveform.remove_cp(tx_pilot_ref, cp_len=ofdm_conf)
+    tx_pilot_no_cp = waveform.remove_cp(tx_pilot_ref, cp_len=ofdm_conf.CP_LEN)
     tx_pilot_freq = waveform.time_to_freq(tx_pilot_no_cp)
     #Calculate Channel Gains
     Lambda_est = CHEST.channel_estimation_calc(rx_pilot_freq=rx_pilot_freq, tx_pilot_ref=tx_pilot_freq, config=ofdm_conf)
