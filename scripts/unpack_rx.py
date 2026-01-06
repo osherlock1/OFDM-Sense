fix scaling

scripts/unpack_rx.py
@@ -193,7 +193,7 @@ def main():
     print(f"BER:{ber*100:.2f}%")
 
     # ---------- Plots ---------------
-    ref_constalation = qam.get_reference_constalation(scale=np.sqrt(10))
+    ref_constalation = qam.get_reference_constalation()
 
     plt.figure()
     plt.scatter(np.real(demodulated_data), np.imag(demodulated_data), alpha=0.5)