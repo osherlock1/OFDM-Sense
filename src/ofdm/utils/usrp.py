728c0da789d33fe0636bdfebd6394328b9dba879
osherlock1 <omalley.j.sherlock@gmail.com>
2026-01-28 22:48:19 -0500

    revent to 100MHz

--- src/ofdm/utils/usrp.py
+++ src/ofdm/utils/usrp.py
@@ -13,8 +13,8 @@ class USRPConfig:
     rx_build_path: str ="./build/RX_TO_FILE"
     tx_addr: str = "addr=192.168.30.2"
     rx_addr: str = "addr=192.168.30.2"
-    tx_rate: float = 20e6
-    rx_rate: float = 20e6
+    tx_rate: float = 100e6
+    rx_rate: float = 100e6
     tx_freq: float = 60e6
     rx_freq: float = 60e6
     wave_type:str = "SINE"
