4659d522aa9bdfea75c51996a6d05940c9a59cd7
osherlock1 <omalley.sherlock@uri.edu>
2026-02-12 19:45:33 -0500

    update with ref and no ref options

--- scripts/delay/run_delay_experiment.py
+++ scripts/delay/run_delay_experiment.py
@@ -12,21 +12,96 @@ import scipy.signal
 import pandas as pd
 import os
 from datetime import datetime
+import argparse
 
-EXPERIMENT_NAME = "single_rx_no_ref"
+EXPERIMENT_NAME = "test"
 EXPERIMENT_PATH = f"./experiments/{EXPERIMENT_NAME}.csv"
+
+DELAY_DATA_PATH = "./metadata/delay_calc.json"
+REF_DELAY_DATA_PATH = "./metadata/ref_delay_calc.json"
+
+
+
+
+
+
 
 
 def main():
     
+    parser = argparse.ArgumentParser()
 
-    print("--- Starting Transfer ----")
-    subprocess.run(["python", "./scripts/run_transfer.py"], check=True)
-    print("--- Transfer Complete ---")
+    parser.add_argument("--ref", action="store_true")
 
-    print("---Calculating Delay -----")
-    subprocess.run(["python", "./scripts/calc_delay.py"], check = True)
-    #run subprocess to calculate delay
+    args = parser.parse_args()
+
+
+
+
+    if (args.ref == True):
+        print("--- Starting Transfer (REF) ----")
+        subprocess.run(["python", "./scripts/run_transfer.py"], check=True)
+        print("--- Transfer Complete ---")
+
+        print("---Calculating Delay -----")
+        subprocess.run(["python", "./scripts/delay/calc_delay.py", "--ref"], check = True)
+        #run subprocess to calculate delay
+
+        with open(REF_DELAY_DATA_PATH, 'r') as f:
+            delay_data = json.load(f)
+
+
+
+        results = {
+            'delay':delay_data["delay"],
+            'distance':delay_data["raw_distance"]
+        }
+
+        df_new = pd.DataFrame([results])
+
+        write_header = not os.path.exists(EXPERIMENT_PATH)
+
+        df_new.to_csv(
+            EXPERIMENT_PATH,
+            mode = 'a',
+            header=write_header,
+            index=False
+        )
+    
+    if (args.ref == False):
+        print("--- Starting Transfer (NO REF) ----")
+        subprocess.run(["python", "./scripts/run_transfer.py"], check=True)
+        print("--- Transfer Complete ---")
+
+        print("---Calculating Delay -----")
+        subprocess.run(["python", "./scripts/delay/calc_delay.py"], check = True)
+        #run subprocess to calculate delay
+
+        with open(DELAY_DATA_PATH, 'r') as f:
+            delay_data = json.load(f)
+
+        delays = delay_data['delays']
+        distances = delay_data['raw_distance']
+
+        results = {
+            'delay0':delays[0],
+            'distance0':distances[0],
+            'delay1':delays[1],
+            'distance1':distances[1]
+        }
+
+        df_new = pd.DataFrame([results])
+
+        write_header = not os.path.exists(EXPERIMENT_PATH)
+
+        df_new.to_csv(
+            EXPERIMENT_PATH,
+            mode = 'a',
+            header=write_header,
+            index=False
+        )
+
+
 
 
 if __name__ == "__main__":
