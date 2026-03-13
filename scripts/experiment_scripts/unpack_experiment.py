00cb0a8633bae1e585ed3d3a16b725896c0b8e26
osherlock1 <omalley.sherlock@uri.edu>
2026-03-13 00:13:43 -0400

    add automation to unpacking .dat files in a directory

--- scripts/experiment_scripts/unpack_experiment.py
+++ scripts/experiment_scripts/unpack_experiment.py
@@ -18,10 +18,19 @@ import argparse
 from ofdm.utils import usrp
 
 
-data_dir = Path("./experiments/synthetic_trilateration1/trilat_rx3_x5_5in_y20_5in_archive/channel0")
+DELAY_DATA_PATH = "./metadata/delay_calc.json"
+REF_DELAY_DATA_PATH = "./metadata/ref_delay_calc.json"
+PERFORMANCE_DATA_PATH = "./data_files/ofdm_performance.json"
+USRP_CONFIG_PATH = "./configs/usrp_settings.yaml"
+UNPACKED_DATA_CSV = "./experiments/unpacked_data/rx3_channel1.csv"
+
+
+
+
+data_dir = Path("/home/guoyixu/OFDM_Sense/EXPERIMENTS/synthetic_trilateration1/trilat_rx3_x5_5in_y20_5in_archive/channel1")
 
 for dat_file in data_dir.glob("*.dat"):
-    
+    print(dat_file)
     file_path = data_dir / dat_file
 
     print("---Unpacking OFDM Data---")
@@ -29,7 +38,44 @@ for dat_file in data_dir.glob("*.dat"):
     print("---Finished Unpacking---")
 
     print("---Calculating Delay -----")
-    subprocess.run(["python", "./scripts/delay/calc_delay.py"], check = True)
+    subprocess.run(["python", "./scripts/delay/calc_delay.py", "--file", str(dat_file)], check = True)
     #run subprocess to calculate delay
-    break
+
+    with open(DELAY_DATA_PATH, 'r') as f:
+        delay_data = json.load(f)
+
+    with open(PERFORMANCE_DATA_PATH, 'r') as f:
+        performance_data = json.load(f)
+
+    evm = performance_data['evm']
+    ber = performance_data['ber']
+    ser = performance_data['ser']
+    starting_idxs = performance_data['start_idx']
+
+    delays = delay_data['delays']
+
+    usrp_conf = usrp.load_config(USRP_CONFIG_PATH)
+    rx_channel_idx = usrp_conf.rx_channel_idx.replace(",", "")
+
+    results = {}
+    channel = 0
+    results.update({
+        f'delay{channel}':delays[int(channel)],
+        f'evm{channel}': evm[int(channel)],
+        f'ber{channel}':ber[int(channel)],
+        f'ser{channel}':ser[int(channel)],
+        f'startingx_idx{channel}' : starting_idxs[int(channel)][1]
+    })
+
+    df_new = pd.DataFrame([results])
+
+    write_header = not os.path.exists(UNPACKED_DATA_CSV)
+
+    df_new.to_csv(
+        UNPACKED_DATA_CSV,
+        mode = 'a',
+        header=write_header,
+        index=False
+    )
+
 
