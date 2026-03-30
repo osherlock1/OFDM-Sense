15c6fd3c40489d7f943111483bbd6e7b11844931
osherlock1 <omalley.sherlock@uri.edu>
2026-03-30 12:57:59 -0400

    update for 3 RX

--- scripts/trilateration.py
+++ scripts/trilateration.py
@@ -10,21 +10,25 @@ C = constants.c
 DELAY_DATA_PATH = "./metadata/delay_calc.json"
 
 RX_COORDS = np.array([
-    [0.0, 0.0], # RX 1
-    [0.508, 0.137], # RX 2
-    [0.0, 0.615], # RX 3
+    [0.0, 0.0], # ANchor
+    [0.4699, 0.08255], # RX 1
+    [0.53975, 0.14605], # RX 2
+    [0.5461, 0.3048] # rx 3
 ])
 
-TX_TRUE = np.array([0.270, 0.970])
+TX_TRUE = np.array([0.565, 0.906])
 
 def load_tdoa_from_csvs():
-    base_dir = "./experiments/unpacked_data3"
+    base_dir = "./experiments/multilat_data1"
     
-    df_rx2_ch0 = pd.read_csv(os.path.join(base_dir, "rx2_channel0.csv"))
-    df_rx2_ch1 = pd.read_csv(os.path.join(base_dir, "rx2_channel1.csv"))
+    df_rx2_ch0 = pd.read_csv(os.path.join(base_dir, "rx1_channel0.csv"))
+    df_rx2_ch1 = pd.read_csv(os.path.join(base_dir, "rx1_channel1.csv"))
     
-    df_rx3_ch0 = pd.read_csv(os.path.join(base_dir, "rx3_channel0.csv"))
-    df_rx3_ch1 = pd.read_csv(os.path.join(base_dir, "rx3_channel1.csv"))
+    df_rx3_ch0 = pd.read_csv(os.path.join(base_dir, "rx2_channel0.csv"))
+    df_rx3_ch1 = pd.read_csv(os.path.join(base_dir, "rx2_channel1.csv"))
+
+    df_rx4_ch0 = pd.read_csv(os.path.join(base_dir, "rx3_channel0.csv"))
+    df_rx4_ch1 = pd.read_csv(os.path.join(base_dir, "rx3_channel1.csv"))
 
     col_name = 'delay0'
 
@@ -33,25 +37,31 @@ def load_tdoa_from_csvs():
     raw_rover_t1_ns  = df_rx2_ch0[col_name].values # Ch 0
     raw_anchor_t2_ns = df_rx3_ch1[col_name].values # Ch 1
     raw_rover_t2_ns  = df_rx3_ch0[col_name].values # Ch 0
+    raw_anchor_t3_ns = df_rx4_ch0[col_name].values # Ch 1
+    raw_rover_t3_ns = df_rx4_ch1[col_name].values # Ch 0
 
     # calc tdoa
     raw_dt_1_ns = raw_rover_t1_ns - raw_anchor_t1_ns
     raw_dt_2_ns = raw_rover_t2_ns - raw_anchor_t2_ns
+    raw_dt_3_ns = raw_rover_t3_ns - raw_anchor_t3_ns
 
     # correct hardware delays
     HARDWARE_BIAS_NS = 3.463
     bias_corrected_dt_1 = raw_dt_1_ns - HARDWARE_BIAS_NS
     bias_corrected_dt_2 = raw_dt_2_ns - HARDWARE_BIAS_NS
+    bias_corrected_dt_3 = raw_dt_3_ns - HARDWARE_BIAS_NS
 
     # correct SDR clock slip
     dt_1_ns = (bias_corrected_dt_1 + 5.0) % 10.0 - 5.0
     dt_2_ns = (bias_corrected_dt_2 + 5.0) % 10.0 - 5.0
+    dt_3_ns = (bias_corrected_dt_3 + 5.0) % 10.0 - 5.0
 
     # convert to seconds
     dt_1_sec = dt_1_ns * 1e-9
     dt_2_sec = dt_2_ns * 1e-9
+    dt_3_sec = dt_3_ns * 1e-9
 
-    return np.column_stack((dt_1_sec, dt_2_sec))
+    return np.column_stack((dt_1_sec, dt_2_sec, dt_3_sec))
 
 
 
