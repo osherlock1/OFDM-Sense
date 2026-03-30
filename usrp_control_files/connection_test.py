cd7e238dbe915fdbb35bc8b069e874b4f39b3b5c
O'Malley Sherlock <omalley.j.sherlock@gmail.com>
2026-03-30 15:18:09 -0400

    Delete usrp_control_files/connection_test.py

--- usrp_control_files/connection_test.py
+++
@@ -1,8 +0,0 @@
-import uhd 
-
-print("Creating device...")
-dlist = 
-print("Found", dlist)
-print("Creating device...")
-usrp = uhd.usrp.MultiUSRP("type=x300,addr=192.168.30.2")
-print("Tx rate now:", usrp.get_tx_rate(0))
