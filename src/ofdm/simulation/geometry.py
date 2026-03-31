ee1286d962b0d5df3521b1a85f5f10cf1d0dd08f
osherlock1 <omalley.sherlock@uri.edu>
2026-03-31 11:17:15 -0400

    add helper function for calculation toa and tdoa

--- src/ofdm/simulation/geometry.py
+++ src/ofdm/simulation/geometry.py
@@ -3,7 +3,13 @@ from scipy import constants
 
 C = constants.c
 
-def ideal_tdoa(tx_pos, rx_coords):
-    distances = np.linalg.norm(rx_coords - tx_pos, axis=1)
-    toa = distances / C
-    return toa[1:] - toa[0]
+def compute_toa(tx_pos, rx_coords):
+    distances = np.linalg.norm(rx_coords - tx_pos, axis = 1)
+    return distances / C
+
+def compute_tdoa(tx_pos, rx_coords):
+    toa = compute_toa(tx_pos, rx_coords)
+    return toa[1:] - toa[0] # roaming rx - anchor
+
+def compute_distances(tx_pos, rx_coords):
+    return np.linalg.norm(rx_coords - tx_pos, axis = 1)
